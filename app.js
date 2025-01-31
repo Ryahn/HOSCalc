a7ab7e96941c7adca2432abc882ebc65b2b0a839
Ryahn <ryancarr10@gmail.com>
2025-01-31 15:13:47 -0600

    add landing

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', require('./routes/index'));
 
+app.use('/', express.static('public'));
+
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
